8edfac3f6dbe935b5112bc842195684e0e546f7d
Jeongyeon Park <ririsid@me.com>
2017-08-12 17:46:31 +0900

    Support all arguments

--- Sources/main.swift
+++ Sources/main.swift
@@ -6,14 +6,26 @@
 //
 //
 
+/// 인자 배열
+let arguments = CommandLine.arguments
+
 // 인자가 있는지 확안한다.
-guard CommandLine.arguments.count > 1 else {
-    fatalError("No arguments!")
+if arguments.count > 1 {
+    /// 변환된 문자열을 저장할 변수
+    var convertedText: String = ""
+
+    // 모든 인자를 변환해서 `convertedText`에 추가한다.
+    for n in 1..<arguments.count {
+        let converted = koLog(arguments[n])
+        if n == 1 {
+            convertedText = converted
+        } else {
+            convertedText = "\(convertedText) \(converted)"
+        }
+    }
+
+    // 최종 결과를 출력한다.
+    print("=> \"\(convertedText)\"")
+} else {
+    print("=> No arguments!")
 }
-
-// 첫 번째 인자를 꺼낸다.
-let arg = CommandLine.arguments[1]
-
-// 인자를 변환하고 화면에 찍는다.
-let newString = koLog(arg)
-print("=> \"\(newString)\"")
