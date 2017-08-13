eab7392ffa77942059e0f6719c3457b548cd1242
Jeongyeon Park <ririsid@me.com>
2017-08-13 18:06:57 +0900

    Linux support

--- Sources/XCKoLog.swift
+++ Sources/XCKoLog.swift
@@ -33,9 +33,13 @@ private func replacingCharacters(with string: String, regex: NSRegularExpression
     }
 
     // 정규표현식 결과를 사용해 변환하려면 `NSString`을 쓰는 게 편하다.
-    let nsString = string as NSString
+    let nsString = NSString(string: string)
     // 찾은 문자를 잘라낸다.
-    let substring = nsString.substring(with: firstMatch.rangeAt(1))
+    #if os(Linux)
+        let substring = nsString.substring(with: firstMatch.range(at: 1))
+    #else
+        let substring = nsString.substring(with: firstMatch.rangeAt(1))
+    #endif
     // 유니코드 값을 구한다. 유니코드 문자열은 16진수이며, 유니코드 스칼라는 21비트이다.
     let unicodeValue = UInt32(substring, radix: 16)!
     // 유니코드 값을 유니코드 스칼라로 변환한다.
@@ -44,7 +48,11 @@ private func replacingCharacters(with string: String, regex: NSRegularExpression
     }
 
     // 유니코드 표현을 찾아 유니코드 스칼라로 교체한다.
-    let newString = nsString.replacingCharacters(in: firstMatch.rangeAt(0), with: String(unicodeScalar))
+    #if os(Linux)
+        let newString = nsString.replacingCharacters(in: firstMatch.range(at: 0), with: String(unicodeScalar))
+    #else
+        let newString = nsString.replacingCharacters(in: firstMatch.rangeAt(0), with: String(unicodeScalar))
+    #endif
 
     // 재귀 호출한다.
     return replacingCharacters(with: newString, regex: regex)
